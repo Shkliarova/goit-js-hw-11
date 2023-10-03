1c281d1129caaff2e78e607ecda4b8ba38689a98
Shkliarova <shklyarovaalisa@gmail.com>
2023-10-04 00:00:30 +0300

    hw-part2

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,14 @@
 import Notiflix from 'notiflix';
 import axios from "axios";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '39799799-b8751a2dca689677868dbdc1d';
 
 const form = document.querySelector('.search-form');
-const input = form.querySelector('[name = "searchQuery"]')
+const input = form.querySelector('[name = "searchQuery"]');
+const gallery = document.querySelector('.gallery');
 
 form.addEventListener('submit', onSubmit);
 
@@ -28,8 +31,46 @@ form.addEventListener('submit', onSubmit);
         }
 
         const data = await response.json();
-        return data;
+
+        if (data.hits.length === 0) {
+            gallery.innerHTML = '';
+            Notiflix.Notify.failure(
+              'Sorry, there are no images matching your search query. Please try again.'
+            );
+          } else {
+            gallery.innerHTML = createMarkup(data.hits);
+
+            new SimpleLightbox('.gallery a', {
+               close: true
+            });
+          }
+
     } catch(error) {
-        Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+        Notiflix.Notify.failure('An error occurred while fetching data. Please try again later.');
     }
 }
+
+function createMarkup(arr){
+    return arr.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => 
+    `<div class="photo-card">
+    <a class = "gallery-link" href = "${largeImageURL}">
+    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+    </a>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes<br> ${likes}</b>
+      </p>
+      <p class="info-item">
+        <b>Views<br> ${views}</b>
+      </p>
+      <p class="info-item">
+        <b>Comments<br> ${comments}</b>
+      </p>
+      <p class="info-item">
+        <b>Downloads<br> ${downloads}</b>
+      </p>
+    </div>
+  </div>`
+    ).join('');
+}
+
