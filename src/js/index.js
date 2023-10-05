86238a37586918060819f06be1e9e6b865da3872
Shkliarova <shklyarovaalisa@gmail.com>
2023-10-05 23:31:45 +0300

    hw-part4

--- src/js/index.js
+++ src/js/index.js
@@ -6,68 +6,94 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '39799799-b8751a2dca689677868dbdc1d';
 
-const form = document.querySelector('.search-form');
-const input = form.querySelector('[name = "searchQuery"]');
-const gallery = document.querySelector('.gallery');
-const button = document.querySelector('.load-more');
+const selectors = {
+  form: document.querySelector('.search-form'),
+  input: document.querySelector('[name = "searchQuery"]'),
+  gallery: document.querySelector('.gallery'),
+  button: document.querySelector('.load-more')
+}
 
-button.style.display = 'none';
+selectors.button.style.display = 'none';
 let page = 1;
 
-form.addEventListener('submit', onSubmit);
-button.addEventListener('click', onButtonClick);
+selectors.form.addEventListener('submit', onSubmit);
+selectors.button.addEventListener('click', onButtonClick);
 
- async function onSubmit(event){
-    event.preventDefault();
-    page = 1;
+async function fetchData(params) {
+  try {
+      const response = await axios.get(BASE_URL, { params });
+      return response.data;
+  } catch (error) {
+    Notiflix.Notify.failure('An error occurred while fetching data. Please try again later.');
+  }
+}
 
-    const params = {
-        key: API_KEY,
-        q: input.value,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        per_page: 40
-    };
+function updateGallery(data){
+  if (data.hits.length === 0) {
+    selectors.gallery.innerHTML = '';
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  } else {
+    selectors.gallery.innerHTML = createMarkup(data.hits);
 
-    try {
-        const response = await axios.get(BASE_URL, { params });
-
-        if(response.status !== 200){
-            throw new Error(response.statusText);
-        }
-
-        const data = response.data;
-
-        if (data.hits.length === 0) {
-            gallery.innerHTML = '';
-            Notiflix.Notify.failure(
-              'Sorry, there are no images matching your search query. Please try again.'
-            );
-          } else {
-            gallery.innerHTML = createMarkup(data.hits);
-
-            if (data.totalHits > page * 40) {
-                button.style.display = 'block';
-            } else {
-                button.style.display = 'none';
-            }
-
-            new SimpleLightbox('.gallery a', {
-               close: true
-            });
-          }
-
-    } catch(error) {
-        Notiflix.Notify.failure('An error occurred while fetching data. Please try again later.');
+    if (data.totalHits > page * 40) {
+        selectors.button.style.display = 'block';
+    } else {
+        selectors.button.style.display = 'none';
     }
+
+    new SimpleLightbox('.gallery a', {
+       close: true
+    });
+  }
+}
+
+async function onSubmit(event){
+  event.preventDefault();
+  page = 1;
+
+  const searchQuery = selectors.input.value.trim();
+
+    if (!searchQuery) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+        return;
+    }
+
+  const params = {
+      key: API_KEY,
+      q: selectors.input.value,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 40,
+  };
+
+  try {
+    const data = await fetchData(params);
+    updateGallery(data);
+  
+    if (data.totalHits > 0) {
+      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    }
+  
+    if (data.totalHits > page * 40) {
+      selectors.button.style.display = 'block';
+    } else {
+      selectors.button.style.display = 'none';
+    }
+  } catch (error) {
+    Notiflix.Notify.failure(error.message);
+  }
 }
 
 function createMarkup(arr){
     return arr.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => 
     `<div class="photo-card">
     <a class = "gallery-link" href = "${largeImageURL}">
-    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+    <img src="${webformatURL}" alt="${tags}" loading="lazy" width = "300" />
     </a>
     <div class="info">
       <p class="info-item">
@@ -88,11 +114,11 @@ function createMarkup(arr){
 }
 
 async function onButtonClick(){
-    page++;
+    page += 1;
 
     const params = {
         key: API_KEY,
-        q: input.value,
+        q: selectors.input.value,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
@@ -101,28 +127,28 @@ async function onButtonClick(){
     };
 
     try {
-        const response = await axios.get(BASE_URL, { params });
+      const data = await fetchData(params);
 
-        if(response.status !== 200){
-            throw new Error(response.statusText);
-        }
+      if (data.hits.length === 0) {
+          selectors.button.style.display = 'none';
+      } else {
+          const newPhotosMarkup = createMarkup(data.hits);
+          selectors.gallery.insertAdjacentHTML('beforeend', newPhotosMarkup);
 
-        const data = response.data;
-
-        if (data.hits.length === 0) {
-            Notiflix.Notify.failure(
+          if (data.totalHits > page * 40) {
+            selectors.button.style.display = 'block';
+          } else {
+            selectors.button.style.display = 'none';
+            Notiflix.Notify.info(
               "We're sorry, but you've reached the end of search results."
             );
-            button.style.display = 'none';
-          } else {
-            gallery.innerHTML += createMarkup(data.hits);
-
-            new SimpleLightbox('.gallery a', {
-               close: true
-            });
           }
 
-    } catch(error) {
-        Notiflix.Notify.failure('An error occurred while fetching data. Please try again later.');
-    }
+          new SimpleLightbox('.gallery a', {
+              close: true
+          });
+      }
+  } catch (error) {
+      Notiflix.Notify.failure(error.message);
+  }
 }
